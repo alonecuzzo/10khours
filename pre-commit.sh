0f0ebb34ed9102c421ffa5098769c9f6a3446b9c
Jabari Bell <jabari.bell@pxlflu.net>
2012-12-27 01:00:19 -0500

    testing pre-commit hook again

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,6 +2,11 @@
 
 # makes sure that code that isn't part of the current commit gets stashed
 git stash -q --keep-index
+FILES_PATTERN='\.(js|coffee)(\..+)?$'
+FORBIDDEN='console.log'
+git diff --cached --name-only | \
+    grep -E $FILES_PATTERN | \
+    GREP_COLOR='4;5;37;41' xargs grep --color --with-filename -n $FORBIDDEN && echo 'COMMIT REJECTED Found "$FORBIDDEN" references. Please remove them before commiting' && exit 1
 ./run_tests.sh
 RESULT=$?
 # pops the code that was stashed
