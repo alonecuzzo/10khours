aa5b4c8c7502934c8f87cc557338e82d7703a3d9
Jabari Bell <jabari.bell@pxlflu.net>
2012-12-27 02:03:50 -0500

    more more shell changes, commented out stashing for now

--- pre-commit.sh
+++ pre-commit.sh
@@ -11,14 +11,20 @@ then
 	    grep -E $FILES_PATTERN | \
 	    GREP_COLOR='37;41' xargs grep --color --with-filename -n $FORBIDDEN && echo 'COMMIT REJECTED Found "$FORBIDDEN" references. Please remove them before commiting' && exit 1
 	echo '**********Passed config.log() test'
+	# run grunt lint check
+	cd build/10k/ && grunt lint
+	RESULT=$?
+	[ $RESULT -ne 0 ] && exit 1
+	cd ../../
 else
 	echo "no js to be committed"
 fi
+
 # makes sure that code that ch isn't part of the current commit gets stashed
-git stash -q --keep-index
+# git stash -q --keep-index
 ./run_tests.sh
 RESULT=$?
 # pops the code that was stashed
-git stash pop -q
+# git stash pop -q
 [ $RESULT -ne 0 ] && exit 1
 exit 0
