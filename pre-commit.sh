0e5278e29ab9c9594b7bd1a7c66de65216b06064
Jabari Bell <jabari.bell@pxlflu.net>
2012-12-27 01:51:46 -0500

    now checking if there are js files in the diff or not

--- pre-commit.sh
+++ pre-commit.sh
@@ -3,11 +3,18 @@
 # checks to see if there are any console.logs out there.
 FILES_PATTERN='\.(js|coffee)(\..+)?$'
 FORBIDDEN='console.log'
-git diff --cached --name-only | \
-    grep -E $FILES_PATTERN | \
-    GREP_COLOR='37;41' xargs grep --color --with-filename -n $FORBIDDEN && echo 'COMMIT REJECTED Found "$FORBIDDEN" references. Please remove them before commiting' && exit 1
-echo '**********Passed config.log() test'
-# makes sure that code that isn't part of the current commit gets stashed
+
+if git diff --cached --name-only | grep -E $FILES_PATTERN 
+then
+	echo "we have js"
+	git diff --cached --name-only | \
+	    grep -E $FILES_PATTERN | \
+	    GREP_COLOR='37;41' xargs grep --color --with-filename -n $FORBIDDEN && echo 'COMMIT REJECTED Found "$FORBIDDEN" references. Please remove them before commiting' && exit 1
+	echo '**********Passed config.log() test'
+else
+	echo "no js to be committed"
+fi
+# makes sure that code that ch isn't part of the current commit gets stashed
 git stash -q --keep-index
 ./run_tests.sh
 RESULT=$?
